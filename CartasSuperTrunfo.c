14df4c0d53d3986f337d572b3ac2b54062088e0e
Matheus Da Silva Amaro <matthewss.miguell29@gmail.com>
2026-02-28 15:45:46 +0000

    adiconei as CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,25 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
+    // Carta 1
+    char estado1;
+    char codigo1[5];
+    char nomeCidade1[50];
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontosTuristicos1;
+
+
+    // Carta 2
+    char estado2;
+    char codigo2[5];
+    char nomeCidade2[50];
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontosTuristicos2;
+
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
